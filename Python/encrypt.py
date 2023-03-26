bf20b763f82d394d803fafd663f2a16a20cf391d
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-03-26 20:10:47 +0200

    Added python pair creation

--- Python/encrypt.py
+++ Python/encrypt.py
@@ -1,6 +1,8 @@
 def encrypt(cleartext, key):
-    # Convert Key into lowercase
+    # Convert Key & cleartext into lowercase
     key = key.lower()
+    cleartext.lower()
+
     # Remove double characters
     key = "".join(dict.fromkeys(key))
 
@@ -9,12 +11,14 @@ def encrypt(cleartext, key):
     for n in key:
         alphabet = alphabet.replace(n, "")
 
+    # Create the encrypt-matrix
     matrix = []
 
     for i in range(5):
         row = []
 
         for j in range(5):
+            # Create a row
             if len(key) > 5 * i + j:
                 row.insert(j, key[5 * i + j])
             else:
@@ -23,6 +27,19 @@ def encrypt(cleartext, key):
         matrix.insert(i, row)
 
     print(matrix)
+
+    # Create character pairs
+    pairs = []
+    for i in range(0, len(cleartext) + 1, 2):
+        if len(cleartext) > (i + 1):
+            if cleartext[i] != cleartext[i + 1]:
+                pairs.insert(i, cleartext[i] + cleartext[i + 1])
+            else:
+                cleartext = cleartext[:i + 1] + "x" + cleartext[i + 1:]
+                pairs.insert(i, cleartext[i] + "x")
+        else:
+            pairs.insert(i, cleartext[i] + "x")
+    print(pairs)
 
 
 if __name__ == "__main__":
